f01d0776bd5efa7f6ca229c39fbd80fdb269dea9
NaTure77 <xnaudrornfl@gmail.com>
2017-07-26 17:13:01 +0900

    Add timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer: MonoBehaviour {
+
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        text.text = "0";
+        StartCoroutine(timerLogic());
+	}
+
+    IEnumerator timerLogic()
+    {
+        float t = 0;
+        while(true)
+        {
+            text.text = t.ToString();
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
