99ca2d07d29a32f14fd7bef80e11da4f21d4c1cc
HwangHyunJin <bbq2277@naver.com>
2017-08-10 01:21:19 +0900

    temp파일이 없습니다
    복구중이므로...기다려주세요

---
+++ Assets/Scripts/MemPiece.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemPiece : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
+
+
+// tag는 MemPiece로 하고, enter -> invalid -> count++ -> text에 표시까지 해서 확인하기
---
+++ Assets/Scripts/Moving_4.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Moving_4 : MonoBehaviour
+{
+
+    public float speed = 3f;
+    public float jumpPower = 5f;
+
+    float horizontalMove;
+    float verticalMove;
+
+    public Vector2 movement;
+    public Rigidbody2D rb2D;
+
+    bool isJumping = false;
+    bool allowJump = false;
+
+    // public Collision other;
+
+
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        // verticalMove = Input.GetAxisRaw("Vertical");
+        // 각 이름은 Edit/Project/Settings/Input에 있다
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (allowJump == true)
+                isJumping = true; // able to jump
+            else // if jump is not allowed
+                return;
+        }
+   }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "blocks")
+            allowJump = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "blocks")
+        {
+            allowJump = true;
+            isJumping = false;
+        }
+
+        else if (other.gameObject.CompareTag("MemPiece"))
+        {
+            other.gameObject.SetActive(false);
+            // count = count + 1;
+            // SetCountText();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+    }
+
+
+
+    void Run()
+    {
+        movement.Set(horizontalMove, 0f); // 3D에서는 h가 x축, v가 z축 이지만
+        // 여기서는 h가 x축, v가 y축 값이다
+
+        movement = movement.normalized * speed * Time.deltaTime;
+        // normalized는 중간 값
+        rb2D.MovePosition(rb2D.position + movement);
+
+
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+
+    }
+
+    void Jump()
+    {
+
+        if (isJumping == false)
+            return;
+
+
+        else // isJumping == true
+        {
+            float jumpPower = 7f;
+            rb2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isJumping = false; // 추락
+            allowJump = false; // 점프키 방지
+
+        }
+
+    }
+
+
+
+
+}
+ 
