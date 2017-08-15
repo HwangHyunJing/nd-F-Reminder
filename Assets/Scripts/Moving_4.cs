5d760ad9b3c2152974d29277bc89f1d32acbb1a5
HwangHyunJin <bbq2277@naver.com>
2017-08-15 20:53:46 +0900

    자연스러운 움직임
    벼룩튀기, 좌우하강, 좌우무한점프
    셋 다 해결했습니다.
    역시 코드는 배껴야 읍읍

---
+++ Assets/Scripts/Moving_4.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Moving_4 : MonoBehaviour
+{
+
+    public float movePower = 1f;
+    public float jumpPower = 1f;
+
+    float horizontalMove;
+    float verticalMove;
+
+    public Vector3 movement;
+    public Rigidbody2D rb2D;
+
+    public SpriteRenderer render;
+
+    bool isJumping = false;
+    bool allowJump = false;
+
+
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        render = gameObject.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        render = gameObject.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        // 각 이름은 Edit/Project/Settings/Input에 있다
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (allowJump == true)
+                isJumping = true; // able to jump
+            else // if jump is not allowed
+                return;
+        }
+   }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "blocks")
+            allowJump = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "blocks")
+        {
+            allowJump = true;
+            isJumping = false;
+        }
+
+        else if (other.gameObject.CompareTag("MemPiece"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+    }
+
+
+
+    void Run()
+    {
+
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            render.flipX = true;
+            moveVelocity = Vector3.left;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            render.flipX = false;
+            moveVelocity = Vector3.right;
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime/1f; // 여기서 움직이는 정도 변경
+
+    }
+
+    void Jump()
+    {
+
+        if (isJumping == false)
+            return;
+
+
+        else // isJumping == true
+        {
+
+            rb2D.velocity = Vector2.zero;
+            Vector2 jumpVelocity = new Vector2 (0, jumpPower);
+            rb2D.AddForce (jumpVelocity/2f, ForceMode2D.Impulse); // 여기서 점프하는 정도 변경
+            
+
+            isJumping = false; // 추락
+            allowJump = false; // 점프키 방지
+
+        }
+
+    }
+
+
+
+
+}
+ 
