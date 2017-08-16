52d028a8a00409b44aee186e577fe0fe708012c9
HwangHyunJin <bbq2277@naver.com>
2017-08-17 00:02:37 +0900

    이슈1번과 2번만
    3번은 다음에

---
+++ Assets/Scripts/CharCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharCamera : MonoBehaviour {
+
+    public GameObject Player;
+
+    // public float offsetX = 0f;
+    // public float offsetY = 0f;
+    public float offsetZ = -0.5f;
+
+    Vector3 cameraPosition;
+
+    /*void Awake()
+    {
+        Puppet = GameObject.FindGameObjectWithTag("Player");
+    }*/
+
+    void Update() // 게임상의 모든 Update를 마친 후 실행되는 최후의 Update
+    {
+        cameraPosition.x = Player.transform.position.x;
+        cameraPosition.y = Player.transform.position.y;
+        cameraPosition.z = Player.transform.position.z + offsetZ;
+
+        transform.position = cameraPosition;
+    }
+}
