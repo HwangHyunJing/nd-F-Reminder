8cdaccd17a2e1ba34c698683309884c3b172d572
enochjung <enoch-jung@naver.com>
2017-08-21 01:17:52 +0900

    switch-coloring v0.01
    · 테스트를 위한 더미 맵 생성
    · GameManger 라는 싱글톤 생성
      ㄴ 특정 색깔의 오브젝트를 활성화/비활성화하는 함수를 담을 예정

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : ScriptableObject {
+
+	private static GameManager _instance = (GameManager)CreateInstance ("GameManager");
+
+	enum OBJECT_COLOR { NULL, RED, BLUE };
+
+	private int inactive_color = (int)OBJECT_COLOR.NULL;
+
+
+	private GameManager() {
+	}
+
+	public static GameManager GetInstance() {
+		return _instance;
+	}
+
+	public void PutLog() {
+		Debug.Log ("YES");
+	}
+}
